cddbbc39efe8a8348bebfb97180ada3d02c385a8
Linura <13700448+linura@users.noreply.github.com>
2020-05-15 13:54:44 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -59,7 +59,7 @@ echo "-"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Installation dependance installation node-dht-sensor"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-npm install node-dht-sensor
+sudo npm install node-dht-sensor
 
 echo 100 > ${PROGRESS_FILE}
 echo "-"
