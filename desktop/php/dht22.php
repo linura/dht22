0ed5e0490f84cab68df08d90ca5c5ddb01ab6d88
LEBANSAIS <c.lebansais@icloud.com>
2020-05-18 22:47:32 +0200

    Update dht22.php

--- desktop/php/dht22.php
+++ desktop/php/dht22.php
@@ -119,7 +119,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
                             <th>{{Nom}}</th>
                             <th>{{Type}}</th>
                             <th>{{Configuration}}</th>
-                            <th>{{Option}}</th>
+                            <th>{{Options}}</th>
                             <th>{{Action}}</th>
                         </tr>
                     </thead>
