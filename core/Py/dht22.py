aae483e02601aa65019a6aa824c7aa6e301714f7
Linura <13700448+linura@users.noreply.github.com>
2020-05-18 16:24:07 +0200

    Update dht22.py

--- core/Py/dht22.py
+++ core/Py/dht22.py
@@ -47,8 +47,8 @@ humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
         if sensor_value ==1 :
-            #print("{0:0.1f}".format(temperature, humidity))
+            print("{0:0.1f}".format(temperature, humidity))
         if sensor_value == 2 :
-            #print("{1:0.1f}".format(temperature, humidity))
+            print("{1:0.1f}".format(temperature, humidity))
     else:
         print 200 
