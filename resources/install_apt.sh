e39d40f96cd5fb70985f3e63e7b72c8decc6076f
LEBANSAIS <c.lebansais@icloud.com>
2021-01-13 23:34:54 +0100

    detetction 64/B2bit

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -5,6 +5,14 @@ echo "+  v1.0"
 echo "+  LEBANSAIS C"
 echo "++++++++++++++++++++++++++++++++++++++"
 
+if $(uname -m | grep '64'); then
+  echo "ARCH: 64-bit"
+  $proc = 1
+else
+  echo "ARCH: 32-bit"
+  $proc = 2
+fi
+
 PROGRESS_FILE=/tmp/dependances_dht22_en_cours
 if [ ! -z $1 ]; then
 	PROGRESS_FILE=$1
@@ -27,33 +35,43 @@ echo "Installation dependance  installation python 3			  "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 #sudo apt-get update
 #sudo apt-get upgrade
+if proc == 2 then
 sudo apt-get install --yes python3.6
+fi
 
 echo 35 > ${PROGRESS_FILE}
 echo "-"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Installation dependance  installation pip					  "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+if proc == 2 then
 sudo apt-get install --yes python3-dev python3-pip
+fi
 
 echo 70 > ${PROGRESS_FILE}
 echo "-"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Installation dependance  mise a jour pip				      "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+if proc == 2 then
 sudo python3 -m pip install --upgrade pip setuptools wheel
+fi
 
 echo 95 > ${PROGRESS_FILE}
 echo "-"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Installation dependance  installation module DHT			  "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+if proc == 2 then
 sudo pip3 install Adafruit_DHT
+fi
 
 echo 100 > ${PROGRESS_FILE}
 echo "-"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Fin de l'installation des dependances DHT					  "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+if proc == 2 then
 sudo chmod -R 755 ${PROGRESS_FILE}
+fi
 rm ${PROGRESS_FILE}
