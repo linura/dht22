47758fb1ec7b6e1cf1c8ba22bd77fe0d1abd9209
Linura <13700448+linura@users.noreply.github.com>
2024-08-17 14:21:23 +0200

    Update dht22.py

--- core/Py/dht22.py
+++ core/Py/dht22.py
@@ -32,6 +32,8 @@ import sys
 import adafruit_dht
 import board
 
+#gestion du numero de broche en fonction du nombre passer en argume 1->board.D1 ...
+
 boardspins = {'0':board.D0,'1':board.D1,'2':board.D2,'3':board.D3,'4':board.D4,'5':board.D5,'6':board.D6,'7':board.D7,'8':board.D8,'9':board.D9,'10':board.D10,'11':board.D11,'12':board.D12,'13':board.D13,'14':board.D14,'15':board.D15,'16':board.D16,'17':board.D17,'18':board.D18,'19':board.D19,'20':board.D20,'21':board.D21,'22':board.D22,'23':board.D23,'24':board.D24,'25':board.D25,'26':board.D26,'27':board.D27}
 
 pin = boardspins[sys.argv[2]] #au format board (board.D20)
